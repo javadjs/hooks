78e5d6e8a0bbaaa044d3a7aeb1274ebd44d48500
javadpr <jjreactjs@gmail.com>
2024-05-31 15:01:31 +0430

    17_spread-operator-in-es6

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import TimeList from "./TimeList.jsx"
 const App = ()=>{ 
 const [title , setTitl] = useState("title");
 const [iscolor , setiscolor] = useState(false);
-const [timeArr , settimeArr] = useState("00:05:52","00:12:50","10:35:11","00:05:52","00:05:52");
+const [timeArr , settimeArr] = useState([]);
 
 
 
@@ -28,14 +28,13 @@ const changecolor = ()=>{
     
 <div className='ttttt'  style={{background:iscolor ? "black" : "white"}}>
 <div className='showtime' style={{color:iscolor ? "white" : "black"}}>
-<Timer  iscolor={iscolor} changecolor={changecolor} />
+<Timer timeArr = {timeArr} settimeArr = {settimeArr} iscolor={iscolor} changecolor={changecolor} />
 </div>
   <h1 style={{color:iscolor ? "white" : "black"}}>
   {title}
   </h1>
 <Loli x={demox} changecolor={changecolor} iscolor={iscolor} />
 
-<TimeList>{timeArr}</TimeList>
 </div>
 
 
