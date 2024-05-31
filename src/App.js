452207818ebed25fbe4efba036c6bd39d2b1316c
javadpr <jjreactjs@gmail.com>
2024-05-31 22:16:56 +0430

    18_react-context

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Loli from './Loli';
 import Timer from './Timer';
 import TimeList from "./TimeList.jsx"
+import { TestContext } from './TestContext.js';
 
 
 
 const App = ()=>{ 
 const [title , setTitl] = useState("title");
 const [iscolor , setiscolor] = useState(false);
-const [timeArr , settimeArr] = useState([]);
+const [timeArr , settimeArr] = useState(["44:55:66"]);
 
 
 
@@ -25,20 +26,24 @@ const changecolor = ()=>{
 
 
   return(
-    
+    <TestContext.Provider value={{
+      timeArr: timeArr,
+      settimeArr : settimeArr
+    }} >
+
+   
 <div className='ttttt'  style={{background:iscolor ? "black" : "white"}}>
 <div className='showtime' style={{color:iscolor ? "white" : "black"}}>
-<Timer timeArr = {timeArr} settimeArr = {settimeArr} iscolor={iscolor} changecolor={changecolor} />
+<Timer  iscolor={iscolor} changecolor={changecolor} />
 </div>
   <h1 style={{color:iscolor ? "white" : "black"}}>
   {title}
   </h1>
 <Loli x={demox} changecolor={changecolor} iscolor={iscolor} />
-
 </div>
 
 
-
+</TestContext.Provider>
   );
  
 }
